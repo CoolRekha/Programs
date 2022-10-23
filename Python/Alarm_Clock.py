2491fc87b0be53de87cb01271684375240792a33
Ronaldo89 <71316604+Ronaldo89@users.noreply.github.com>
2022-10-23 20:19:14 +0200

    Update Alarm_Clock.py
    added comments and the required libraries that need to be installed

--- Python/Alarm_Clock.py
+++ Python/Alarm_Clock.py
@@ -1,6 +1,9 @@
+#install the required libraries
+!pip install playsound 
+!pip install pygobject
 from datetime import datetime   
 from playsound import playsound
-alarm_time = input("Enter the time of alarm to be set:HH:MM:SS\n")
+alarm_time = input("Enter the time of alarm to be set:HH:MM:SS\n")#user enters the time 
 alarm_hour=alarm_time[0:2]
 alarm_minute=alarm_time[3:5]
 alarm_seconds=alarm_time[6:8]
